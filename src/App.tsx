522cc24285e61507d7466fe5f5e4bc93ad0f2203
Regulusarm <forever.alinur@gmail.com>
2022-09-14 22:39:41 +0600

    added Ayau

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import {
   ChakraProvider,
   Box,
   theme,
+  chakra,
+  Flex,
 } from "@chakra-ui/react"
 import WithSubnavigation from "./components/WithSubnavigation"
+import logo from "./ayau.jpg"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <WithSubnavigation />
+      <Flex justifyContent={'center'} alignItems={'center'}>
+       <chakra.img src={logo} />
+      </Flex>
     </Box>
   </ChakraProvider>
 )
