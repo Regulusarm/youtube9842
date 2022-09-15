6a2b7fc29add4c09672905ac4585dc6e332816a4
Regulusarm <Forever.alinur@gmail.com>
2022-09-15 18:40:39 +0600

    addded youtube player

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,67 @@ import {
   ChakraProvider,
   Box,
   theme,
-  chakra,
   Flex,
+  Stack,
+  Button,
 } from "@chakra-ui/react"
-import WithSubnavigation from "./components/WithSubnavigation"
-import logo from "./ayau.jpg"
+import { useEffect } from "react";
+import WithSubnavigation from "./components/WithSubnavigation";
+import YouTubePlayer from 'youtube-player';
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
+export const App = () => {
+  let player: any; 
+
+  function createPlayer() { 
+    player = YouTubePlayer('player', {
+    height: '360',
+    width: '640',
+    videoId: '0eIY5b0RKE0',
+    playerVars: {
+      controls: 0,
+      disablekb: 1,
+      modestbranding: 1,
+      rel: 0,
+    }
+    });
+    player.mute();
+    player.playVideo();
+  }
+
+  function play() {
+    let video: any
+    video = document.getElementById("player")
+    video.contentWindow.postMessage( '{"event":"command", "func":"playVideo", "args":""}', '*');
+ }
+
+  function pause() {
+    let video: any
+    video = document.getElementById("player")
+    video.contentWindow.postMessage( '{"event":"command", "func":"pauseVideo", "args":""}', '*');
+ }
+
+  useEffect(() => {
+    createPlayer()
+    
+  }, [])
+
+   
+  return <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <WithSubnavigation />
-      <Flex justifyContent={'center'} alignItems={'center'}>
-       <chakra.img src={logo} />
+      <Flex justifyContent={'center'} alignItems={'center'} flexDirection={'column'}>
+        <Flex justifyContent={'center'} alignItems={'start'} flexDirection={'column'}>
+          <div id="player"></div>
+          <Stack spacing={4} direction='row' mt={2} align='start'>
+            <Button colorScheme='teal' size='sm' onClick={() => play()}>
+              Play
+            </Button>
+            <Button colorScheme='teal' size='sm' onClick={() => pause() }>
+              Pause
+            </Button>
+          </Stack>
+        </Flex>
       </Flex>
     </Box>
   </ChakraProvider>
-)
+}
